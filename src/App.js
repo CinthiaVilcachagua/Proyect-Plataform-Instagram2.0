b3940bfd1aa6f31f0a6f31dfe818ec89f410acc6
CinthiaVilcachagua <cv.ccori@gmail.com>
2019-11-28 16:32:47 -0500

    creando el objeto user, modificando el estado, mostrando los datos del usurio como foto, nombre y correo. Añadiendo un boton para cerrar sesion.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,25 @@ import firebase from 'firebase'
 import './App.css'
 
 class App extends Component {
+   //Definiendo el estado.
+   constructor() {
+      super()
+      this.state = {
+         user: null
+      }
+
+      this.handleAuth = this.handleAuth.bind(this)
+      this.handleLogout = this.handleLogout.bind(this)
+   }
+
+   componentWillMount() {
+      firebase.auth().onAuthStateChanged((user) => {
+         this.setState({
+            user: user
+         })
+      })
+   }
+
    //creando la funcion para loguearse con google.
    handleAuth() {
       const provider = new firebase.auth.GoogleAuthProvider()
@@ -15,6 +34,31 @@ class App extends Component {
          .catch((error) => console.log(`Error ${error.code}: ${error.message}`))
    }
 
+   //creando la funcion para cerrar sesion.
+   handleLogout() {
+      firebase
+         .auth()
+         .signOut()
+         .then((result) => console.log(`${result.user.email} ha cerrado sesion`))
+         .catch((error) => console.log(`Error ${error.code}: ${error.message}`))
+   }
+
+   renderLoginInButton() {
+      //Si el usuario esta logueado. si ... es distinto de null
+      if (this.state.user) {
+         return (
+            <div>
+               <img width="100" src={this.state.user.photoURL} alt={this.state.user.displayName} />
+               <p>Hola {this.state.user.displayName}</p>
+               <button onClick={this.handleLogout}>Cerrar sesion</button>
+            </div>
+         )
+      } else {
+         //si no esta logueado. invocaremos al boton que es para loguearse
+         return <button onClick={this.handleAuth}>Login con Google</button>
+      }
+   }
+
    render() {
       return (
          <div className="App">
@@ -22,7 +66,8 @@ class App extends Component {
                <h2>Pseudogram</h2>
             </div>
             <p className="App-intro">
-               <button onClick={this.handleAuth}>Login con Google</button>
+               {this.renderLoginInButton()}
+               {/*<button onClick={this.handleAuth}>Login con Google</button>*/}
             </p>
          </div>
       )
